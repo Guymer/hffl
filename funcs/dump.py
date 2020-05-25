b971025378b6ac5a3df50e269c0ce342e409bdb7
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-05-25 12:33:58 +0100

    added function to print out the exterior ring of a Polygon

---
+++ funcs/dump.py
@@ -0,0 +1,21 @@
+def dump(poly):
+    # Import special modules ...
+    try:
+        import shapely
+        import shapely.geometry
+    except:
+        raise Exception("run \"pip install --user shapely\"")
+
+    # Check argument ...
+    if not isinstance(poly, shapely.geometry.polygon.Polygon):
+        raise TypeError("\"poly\" is not a Polygon")
+
+    # Open CSV file ...
+    with open("error.csv", "wt") as fobj:
+        # Write header ...
+        fobj.write("lon [°],lat [°]\n")
+
+        # Loop over coordinates in external ring ...
+        for lon, lat in poly.exterior.coords:
+            # Write data ...
+            fobj.write("{:.12f},{:.12f}\n".format(lon, lat))
