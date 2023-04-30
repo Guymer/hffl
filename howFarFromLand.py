dc19b18bf05dc17d25a33b333f24f1d829710b9e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-04-30 07:29:51 -0400

    don't specify defaults

--- howFarFromLand.py
+++ howFarFromLand.py
@@ -266,7 +266,7 @@ if __name__ == "__main__":
         )
 
         # Configure axis ...
-        pyguymer3.geo.add_coastlines(ax, resolution = "i")
+        pyguymer3.geo.add_coastlines(ax)
 
         # Deduce GeoJSON name ...
         fname = f"{stub}.geojson"
